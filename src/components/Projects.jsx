276b89700a289bda96a3831f4c9f31dedf000ce2
Tanmay K. Sawwalakhe <tkdsawwalakhe01@gmail.com>
2024-07-11 21:12:17 +0530

    added more animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,27 +1,57 @@
-import React from 'react'
+import React from 'react';
 import { PROJECTS } from '../constants';
+import { motion } from 'framer-motion';
 
 const Projects = () => {
-  return (
-    <div className="border-b border-neutral-900 pb-4 ">
-        <h2 className="my-20 text-center text-4xl">Projects</h2>
-        <div className="">{PROJECTS.map((projects , index) =>(
-            <div className="mb-8 flex flex-wrap lg:justify-center" key={index}>
-                <div className="w-full lg:w-1/4" >
-                <img src={projects.image} width={150} height={150} alt={projects.title} className="mb-6 rounded" />
-            </div>
-            <div className="w-full max-w-xl lg:w-3/4">
-                <h6 className=" mb-2 font-semibold">{projects.title}</h6>
-                <p className="mb-4 text-neutral-400">{projects.description}</p>
-                {projects.technologies.map((tech , index) =>(
-                    <span className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800" key={index}>{tech}</span>
+    return (
+        <div className="border-b border-neutral-900 pb-4">
+            <motion.h2
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 0.5 }}
+                className="my-20 text-center text-4xl"
+            >
+                Projects
+            </motion.h2>
+            <div>
+                {PROJECTS.map((project, index) => (
+                    <div className="mb-8 flex flex-wrap lg:justify-center" key={index}>
+                        <motion.div
+                            whileInView={{ opacity: 1, x: 0 }}
+                            initial={{ opacity: 0, x: -100 }}
+                            transition={{ duration: 0.5 }}
+                            className="w-full lg:w-1/4"
+                        >
+                            <img
+                                src={project.image}
+                                width={150}
+                                height={150}
+                                alt={project.title}
+                                className="mb-6 rounded"
+                            />
+                        </motion.div>
+                        <motion.div
+                            whileInView={{ opacity: 1, x: 0 }}
+                            initial={{ opacity: 0, x: 100 }}
+                            transition={{ duration: 0.5 }}
+                            className="w-full max-w-xl lg:w-3/4"
+                        >
+                            <h6 className="mb-2 font-semibold">{project.title}</h6>
+                            <p className="mb-4 text-neutral-400">{project.description}</p>
+                            {project.technologies.map((tech, techIndex) => (
+                                <span
+                                    className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
+                                    key={techIndex}
+                                >
+                                    {tech}
+                                </span>
+                            ))}
+                        </motion.div>
+                    </div>
                 ))}
             </div>
-            </div>
-        ))}
         </div>
-    </div>
-  )
+    );
 }
 
 export default Projects;
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,32 +5,83 @@ import { SiExpress } from 'react-icons/si';
 import { FaNodeJs } from 'react-icons/fa';
 import { BsGithub } from 'react-icons/bs';
 import { DiJavascript1 } from 'react-icons/di'; 
+import { motion } from 'framer-motion';
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse"
+    }
+  }
+});
+
 
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-        <h2 className="my-20 text-center text-4xl">Tech Stack</h2>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-          <div className="rounded-2xl border-4 border-neutral-800 p-3">
-            <SiMongodb className="text-5xl text-green-500" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-3">
+        <motion.h2
+        whileInView={{opacity:1, y:0}}
+        initial={{opacity:0, y:-100}}
+        transition={{duration:1.5}}
+        className="my-20 text-center text-4xl">Tech Stack</motion.h2>
+        <motion.div
+        whileInView={{opacity:1, x:0}}
+        initial={{opacity:0, x:-100}}
+        transition={{duration:1.5}}
+        className="flex flex-wrap items-center justify-center gap-4">
+          <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-3">
+            <SiMongodb
+            variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+            className="text-5xl text-green-500" />
+          </motion.div>
+          <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-3">
             <SiExpress className="text-5xl text-400" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-3">
+          </motion.div>
+          <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-3">
             <RiReactjsLine className="text-5xl text-cyan-400" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-3">
+          </motion.div>
+          <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-3">
             <FaNodeJs className="text-5xl text-green-500" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-3">
+          </motion.div>
+          <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-3">
             <DiJavascript1 className="text-5xl text-yellow-400" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-3">
+          </motion.div>
+          <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-3">
             <BsGithub className="text-5xl text-400" />
-          </div>
+          </motion.div>
 
-        </div>
+        </motion.div>
     </div>
   )
 }
