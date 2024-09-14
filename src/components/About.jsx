cf136b30c489e834db0490b31c76be1497b06746
Tanmay K. Sawwalakhe <88837756+tanmay250501@users.noreply.github.com>
2024-09-14 23:05:21 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,7 @@ import aboutImg from "../assets/about.jpg";
 import { ABOUT_TEXT } from '../constants';
 import { motion } from 'framer-motion';
 
+
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4" >
