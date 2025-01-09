0f354546caca3bc5cc3959f5afcbf4855897a765
Tanmay K. Sawwalakhe <88837756+tanmay250501@users.noreply.github.com>
2025-01-09 11:50:32 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,5 @@
 import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
 
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 1 years of hands-on experience, I have honed my skills in front-end technologies like React.js , as well as back-end technologies like Node.js, Express.js, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
